8d013e7349e1792ad6fda51ef586ee1ea504d326
Chiara Passaro <chiarapassaro.wd@gmail.com>
2020-01-27 10:39:56 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,32 @@ $(document).ready(function () {
   $('.icon-send').click(function () {
     sendMessage();
   });
+
+  $('.send-message').keypress(function(event) {
+    if(event.which == 13) {
+      sendMessage();
+    }
+  });
+
+  $('.contact-search input').keyup(function () {
+    var text = $('.contact-search input').val().toLowerCase();
+
+    $('.contact-element').each(function () {
+      var contactName = $(this).find('.contact-name').text().toLowerCase();
+      if(contactName.includes(text) == true) {
+        // console.log('incluso');
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
 });
 
+
+// ------------- FUNZIONI -------------//
+
+// funzione che invia messaggio utente
 function sendMessage() {
   var textMessage = $('input.send-message').val();
 
@@ -20,12 +44,34 @@ function sendMessage() {
 
     newMessage.find('.message-time').text(time);
     newMessage.addClass('sent');
-    $('.col-right-messages').append(newMessage);
-
+    $('.col-right-messages.active').append(newMessage);
+    scrollMessage();
+    setTimeout(sendResponse, 1000);
     $('input.send-message').val('');
   }
 }
 
+// funzione che manda risposta
+function sendResponse() {
+  var messageResponse = $('.template .message').clone();
+  messageResponse.find('.message-text').text('ok');
+  var data = new Date();
+  var hours = addZero(data.getHours());
+  var minutes = addZero(data.getMinutes());
+  var time = hours +':'+ minutes;
+  messageResponse.find('.message-time').text(time);
+  messageResponse.addClass('received');
+  $('.col-right-messages.active').append(messageResponse);
+  scrollMessage();
+}
+
+// funzione che scrolla
+function scrollMessage() {
+    var heightContainer = $('.col-right-messages.active').height();
+    $('.col-right-messages.active').scrollTop(heightContainer);
+}
+
+// Funzione che aggiunge zero
 function addZero(number) {
   if(number < 10) {
     number = '0' + number;
