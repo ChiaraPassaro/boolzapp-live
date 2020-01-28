f63afd3ff844b560b8512bb35898605ba255f130
Chiara Passaro <chiarapassaro.wd@gmail.com>
2020-01-28 10:55:49 +0100

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -22,6 +22,50 @@ $(document).ready(function () {
       }
     });
   });
+
+  // $(document).on('click', '.message-options', function() {
+  //   $('.message-link.active').removeClass('active');
+  //   $('.message-options.active').removeClass('active');
+  //   $(this).addClass('active');
+  //   $(this).parent().siblings('.message-link').addClass('active');
+  // });
+  // $(document).on('click', '.message-options.active', function() {
+    //   console.log('non sono active');
+    //   $(this).parent().siblings('.message-link').removeClass('active');
+    // });
+
+  $(document).on('click', '.message-options', function() {
+    $(this).parent().siblings('.message-link').toggleClass('active');
+  });
+
+  $(document).on('click', '.message-delete', function() {
+    // $(this).parent().parent().parent().remove();
+    $(this).parents('.message').remove();
+  });
+
+  $(document).on('click', '.contact-element', function() {
+    var data = $(this).attr('data-contact');
+    var selector = '.col-right-messages[data-contact="' + data + '"]';
+
+    $('.col-right-messages').removeClass('active');
+    $(selector).addClass('active');
+    $('.contact-element').removeClass('active');
+    $(this).addClass('active');
+
+    var name = $(this).find('.contact-name').text();
+    var time = $(this).find('.contact-time').text();
+    var img = $(this).find('.avatar img').attr('src');
+    $('.col-right .header .contact-active .contact-name').text(name);
+    $('.col-right .header .contact-active .contact-time').text(time);
+    $('.col-right .header .avatar img').attr('src', img);
+  });
+
+  $('.send-message').focus(function(){
+    $('.icon-send i').removeClass('fa fa-microphone').addClass('fas fa-paper-plane');
+  }).blur(function(){
+    $('.icon-send i').removeClass('fas fa-paper-plane').addClass('fa fa-microphone');
+  });
+
 });
 
 
@@ -47,6 +91,7 @@ function sendMessage() {
     $('.col-right-messages.active').append(newMessage);
     scrollMessage();
     setTimeout(sendResponse, 1000);
+
     $('input.send-message').val('');
   }
 }
@@ -55,11 +100,13 @@ function sendMessage() {
 function sendResponse() {
   var messageResponse = $('.template .message').clone();
   messageResponse.find('.message-text').text('ok');
+
   var data = new Date();
   var hours = addZero(data.getHours());
   var minutes = addZero(data.getMinutes());
   var time = hours +':'+ minutes;
   messageResponse.find('.message-time').text(time);
+
   messageResponse.addClass('received');
   $('.col-right-messages.active').append(messageResponse);
   scrollMessage();
@@ -68,10 +115,10 @@ function sendResponse() {
 // funzione che scrolla
 function scrollMessage() {
    // altezza elemento conversazione attiva
-    var heightContainer = $('.col-right-messages.active').height();
-    console.log(heightContainer);
-    // spostiamo scroll container di tutte le conversazioni
-    $('.messages-wrapper').scrollTop(heightContainer);
+  var heightContainer = $('.col-right-messages.active').height();
+  console.log(heightContainer);
+  // spostiamo scroll container di tutte le conversazioni
+  $('.messages-wrapper').scrollTop(heightContainer);
 }
 
 // Funzione che aggiunge zero
